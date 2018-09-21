a53ff4f24a80354187c0631d607d94bc9236fdca
Dave Clay <daveclay@gmail.com>
2018-09-21 13:14:59 -0700

    Configure excerpt size via SiteSettings

--- plugin.rb
+++ plugin.rb
@@ -3,9 +3,11 @@
 # version: 0.2
 # authors: Angus McLeod, Johani
 
+enabled_site_setting :topic_excerpt_length
+
 after_initialize do
   add_to_class :post, :excerpt_for_topic do
-      Post.excerpt(cooked, 50, strip_links: true)
+      Post.excerpt(cooked, SiteSetting.topic_excerpt_length, strip_links: true)
   end
   add_to_serializer(:listable_topic, :include_excerpt?) { true }
 end
